e1d481de089ec960313c78a5836469d68d1ee39f
bhuvanesh25 <bhuvaneshwar.solanki@gmail.com>
2024-10-14 10:16:05 +0530

    Add files via upload

---
+++ task-management-frontend/src/Utils/notifications.jsx
@@ -0,0 +1,16 @@
+export const errorMsg = {
+    msg: "Something went wrong, Please try again later.",
+    css: "p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+};
+export const successMsg = {
+    msg: "Task saved successfully.",
+    css: "p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+}
+export const updateMsg = {
+    msg: "Task updated successfully.",
+    css: "p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+}
+export const deleteMsg = {
+    msg: "Task deleted successfully.",
+    css: "p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300"
+}
